b8f0271910f1d852ff85a6c4d98c10ea2214df65
squiddude38 <squiddude38@gmail.com>
2023-04-02 18:05:10 +1200

    added models and db

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DB_NAME = 'AskIT.db'
+
+   
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile("../config.py")
+    app.config.from_pyfile("../config.py") 
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    from .models import UserSubject,Save,Vote,User,Subject,Question,Response
+
+
 
     from .home import home
     app.register_blueprint(home, url_prefix='/')
---
+++ app/models.py
@@ -0,0 +1,79 @@
+
+from . import db
+#could use faker to gen fake data
+
+UserSubject = db.Table("UserSubject",
+  db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('subject_id', db.Integer, db.ForeignKey('subject.id'), primary_key=True)
+)
+
+Save = db.Table("Save",
+  db.Column('question_id', db.Integer, db.ForeignKey('question.id'), primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+)
+
+#association object
+class Vote(db.Model):
+    __tablename__ ="Vote"
+    
+    user_id= db.Column( db.Integer, db.ForeignKey('user.id'))
+    response_id=db.Column( db.Integer, db.ForeignKey('response.id'))
+    state = db.Column(db.Boolean, nullable=False)
+    db.PrimaryKeyConstraint(user_id, response_id)
+
+
+    user = db.relationship("User", backref="votes")
+    response = db.relationship("Response", backref="votes")
+
+#https://stackoverflow.com/questions/24872541/could-not-assemble-any-primary-key-columns-for-mapped-table
+
+
+# User table database model
+class User(db.Model):
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+    verified = db.Column(db.Boolean, default=False, nullable=False)
+
+    saved = db.relationship('Question', secondary=Save, backref="users_saved")
+
+    
+
+class Subject(db.Model):
+    __tablename__ = "subject"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    users = db.relationship('User', secondary=UserSubject, backref="subjects")
+    
+
+class Question(db.Model):
+    __tablename__ = "question"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    subject_id =db.Column(db.Integer,db.ForeignKey('subject.id'))
+    title = db.Column(db.String(),nullable=False)
+    date_posted = db.Column(db.DateTime,nullable=False)
+    body = db.Column(db.String(),nullable=False)
+    views = db.Column(db.Integer())
+
+    user = db.relationship('User', backref='questions')
+    subject =db.relationship('Subject', backref='questions')
+
+
+class Response(db.Model):
+    __tablename__ = "response"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    question_id =db.Column(db.Integer,db.ForeignKey('question.id'))
+    date_posted = db.Column(db.DateTime,nullable=False)
+    body = db.Column(db.String(),nullable=False)
+    parent_id = db.Column(db.Integer, db.ForeignKey('response.id'))
+    
+
+    user= db.relationship('User', backref='responses')
+    question=db.relationship('Question', backref='responses')
+    replies = db.relationship('Response', backref=db.backref('parent', remote_side=[id]),lazy='dynamic')
+  
